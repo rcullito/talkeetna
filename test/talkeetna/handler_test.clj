d75ea331d45b51b616d8ddc9ce2766ab90c701c6
Rob Culliton <rob.culliton@gmail.com>
2020-06-22 18:54:49 -0400

    test for invalid post

--- test/talkeetna/handler_test.clj
+++ test/talkeetna/handler_test.clj
@@ -11,7 +11,13 @@
           response (app (-> (mock/request :post "/records")
                             (mock/content-type "application/json")
                             (mock/body (cheshire/generate-string post-data))))]
-      (is (= (:status response) 200))))
+      (is (= 200 (:status response)))))
+  (testing "improperly formatted post throws an error"
+    (let [invalid-post-data {:martian "Aaberg, Hillary, Female, Blue, 02/08/1987"}
+          response (app (-> (mock/request :post "/records")
+                            (mock/content-type "application/json")
+                            (mock/body (cheshire/generate-string invalid-post-data))))]
+      (is (= 400 (:status response)))))
   (testing "get should return multiple records from disk as well as any record that has been posted"
     (let [response (app (mock/request :get "/records/gender"))
           returned-data (cheshire/parse-string (:body response))]
@@ -19,7 +25,6 @@
       (is (> (count returned-data) 3))
       ;; check that we have the record we just posted
       (is (some? (some #(= "Chelsea" (get % "first-name")) returned-data)))))
-  
   (testing "not-found"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
