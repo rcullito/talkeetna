43230a603e901de45aabc5dc5ccef220cba1e999
Rob Culliton <rob.culliton@gmail.com>
2020-06-22 15:40:38 -0400

    first rewrite at the handler. not immediately clear if an atom is needed to store state. we shall see

--- src/talkeetna/handler.clj
+++ src/talkeetna/handler.clj
@@ -3,34 +3,30 @@
             [compojure.route :as route]
             [ring.middleware.json :as middleware]
             [compojure.handler :as handler]
-            [talkeetna.core :as c]
-            [talkeetna.input :as i]))
+            [talkeetna.sort :as so]
+            [talkeetna.input :as i]
+            [talkeetna.handler-utils :as hu]))
 
 
 ;; records via POST route will be stored in memory
-(def posted-records (atom ()))
+;; seed the 'working db' with values from our 3 delimited files
+(def posted-records (hu/build-records '("people.csv" "people.ssv" "people.psv")))
 
 (defn process-posted-person [person]
-  "perform operations from core on a single posted record and store in atom"
-  (-> (content->delimiter person)
-      ;; TODO we should not have to do all of these here in the handler
-      ;; this should delegate to core
+  "perform similar set of operations as core's parse-file, though now just for one record and no file handling"
+  (-> (i/select-delimiter-from-input person)
       (i/split-record person)
       i/trim-record
       i/namevec->map
-      (->> (swap! posted-records conj))))
-
-(defn total-records []
-  "HTTP GETS should return stored results on file and POSTed records held in memory. this should update on every request"
-  (into @posted-records file-records))
+      (->> (conj posted-records))))
 
 (defroutes app-routes
   (GET "/records/gender" []
-       (c/sort-gender-last-name (total-records)))
+       (so/sort-gender-last-name posted-records))
   (GET "/records/birthdate" []
-       (c/sort-dob (total-records)))
+       (so/sort-dob posted-records))
   (GET "/records/name" []
-       (c/sort-last-name-desc (total-records)))
+       (so/sort-last-name-desc posted-records))
   (POST "/records" request
         (process-posted-person (get-in request [:body :person])))
   (route/not-found "Not Found"))
